ee422235453c696261e1474757a11134d8fc117a
Jonathan Woolley <jonathan@jpwoolley.co.uk>
2022-12-07 17:38:20 +0000

    removed unused files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # epicFreeGamesNotifierDiscordBot
 A Discord bot which notifies your Discord server about the current free weekly games on the Epic Store.
+
+To use this bot, you'll need to create a config.json file in the same folder as index.js with the following keys:
+* token - the bot token
+* clientId - the clientID of the bot
+* guildId - the ID of the guild you wish the bot to post in
+* channelId - the ID of the channel you wish to post in
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// Require the necessary discord.js classes
+// The section of requirements
 const { Client, EmbedBuilder, Events, GatewayIntentBits } = require('discord.js');
 const { token, guildId, channelId } = require('./config.json');
 const cron = require('cron');
 
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
